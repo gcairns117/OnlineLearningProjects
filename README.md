4ee2181022138f8e2d09f66a257e0cdf4b7d303e
gcairns117 <glenncairns96@gmail.com>
2022-01-26 22:11:24 +0000

    restored files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Online Learning, Courses & Tutorials
+
+Projects and files created in this repo were made while learning alongside online courses and tutorials from the following:
+
+Udemy link : "https://www.udemy.com/course/mini-python-projects/" - Course Created by : Shambhavi Gupta
+
+
+Projects / Files included in repo
+- turtMod: Using Python's turtle module to create a graphic animation
+- converter: Using moviepy module to edit a .mov file and convert it into a .gif
---
+++ converter.py
@@ -0,0 +1,19 @@
+"""
+https://www.udemy.com/course/mini-python-projects/
+https://www.geeksforgeeks.org/introduction-to-moviepy/
+pip install moviepy
+converting .mp4/mov file to .gif file
+moviepy module - used for video editing. It can read and write most common video formats, including GIF
+"""
+
+# #import everthing needed to edit video clips
+# from moviepy.editor import *
+
+# #load video (video.mov file has not been uploaded to git)
+# vid =   VideoFileClip("video.mov").subclip((10.00),(20.00))     #selecting video clip from 10seconds to 20seconds
+        
+# #print the length of the video
+# print(str(vid.duration))
+
+# #save vid as GIF file
+# vid.write_gif("birds.gif")
