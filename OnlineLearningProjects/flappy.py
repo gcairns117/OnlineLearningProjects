9d03b96c2513e509a08a40372a385151287f7021
gcairns117 <glenncairns96@gmail.com>
2022-01-29 00:54:34 +0000

    flappydev progress & converter restored

--- OnlineLearningProjects/flappy.py
+++ OnlineLearningProjects/flappy.py
@@ -1,7 +1,10 @@
 """
-27/01/22
+PyGame Documentation - https://www.pygame.org/docs/
 Icon attribution link  - <a href="https://www.flaticon.com/free-icons/bird" title="bird icons">Bird icons created by Freepik - Flaticon</a>
+PyGame Tutorial - https://www.youtube.com/watch?v=FfWpgLFMI7w&t=964s
+PyGame Primer Guide - https://realpython.com/pygame-a-primer/
 """
+
 # Import pygame moduel 
 import pygame
 
@@ -19,29 +22,59 @@ from pygame.locals import (
 # Inialise pygame modules - required at start of pygame file
 pygame.init()
 
-# Creating the Scrren / drawing the display window
-screen = pygame.display.set_mode((800,800))            # screen size set with tuple, 800x800 pixels
+# Screen dimensions defined as constants
+SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 480
+
+# Creating the Screen / drawing the display window
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))            # screen size set with tuple, 720x480 pixels
 # Setting the Title and Icon
 pygame.display.set_caption("Flappy Imitation")
 icon = pygame.image.load("dove.png")                   # load in dove.png and assing it to 'icon'.
 pygame.display.set_icon(icon)
 
-# Game Loop (aka Main Loop)
-running = True
-while running:
+# Player object - defined by extedning Sprite class
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255,255,255))
+        self.rect = self.surf.get_rect()
+# Instantiate player
+player = Player()
 
-    # Did user click the window close button?
+# Game Loop
+running = True      # Bool control variable to keep game loop running
+while running:      # Starts the event handler
+
+    
+    # Loop through every active event
     for event in pygame.event.get():
+        # Did user click the window close button? If so, stop the loop
         if event.type == QUIT:
             running = False
+        # Did user press a key?
+        elif event.type == KEYDOWN:
+            # Did user press Escape key? If so, stop the loop
+            if event.key == K_ESCAPE:
+                running = False
 
     # Fill the background with light blue for sky
     screen.fill((173, 216, 230))                        # takes tuple rgb colour value
 
-    # Draw a solid green circle in the center
-    pygame.draw.circle(screen, (0, 128, 0), (400, 400), 20)    #aruguement for where to draw, rgb value colour, where centre will be, and radius of circle. 
+    # Create a Surface, pass a tuple containing length and width
+    surf = pygame.Surface((50,50))
+    # Give surface colour
+    surf.fill((0,0,0))
+    rect = surf.get_rect()
+    
+    # Draw surface onto the screen
+    # Placed at centre by subtracting width & height of surf from size of screen and then dividing by 2
+    # Required to find centre as blit() places top left corner of surf at location given
+    screen.blit(player.surf, player.rect)
+    screen.blit(surf, ((SCREEN_WIDTH - surf.get_width())/2,(SCREEN_HEIGHT - player.surf.get_height())/2))
 
-    # Flip the display
+    # Flip the display - updates entire screen with everything that has been drawn since last flip
     pygame.display.flip()
 
 # Once loop is done - quit
--- OnlineLearningProjects/game.py
+++ OnlineLearningProjects/game.py
@@ -4,6 +4,8 @@ https://realpython.com/lessons/basic-pygame-program/
 """
 
 # Import and initailize the pygame library
+# init() function calls the separate init functions of all included pygame modules
+# Pygame Modules are abstractions for specific hardware, so this step is required so that same code can work on Linux, Windows, & Mac
 import pygame
 pygame.init()
 
@@ -11,11 +13,10 @@ pygame.init()
 screen = pygame.display.set_mode((600, 600))
 
 # Run until the user asks to quit
-#Game Loop (aka Main Loop)
-# Game loop does 3 things: Handle events, update the game state, draws game state to the screen
+# Game Loop (aka Main Loop)
+# Game loop does 3 things: Handle events (process inputs), update the game state/world, draws game state to the screen (generate outputs)
 running = True
 while running:
-
     # Did user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,7 +28,7 @@ while running:
     # Draw a solid blue circle in the center
     pygame.draw.circle(screen, (0, 128, 0), (300, 300), 50)    #aruguement for where to draw, rgb value colour, where centre will be, and radius of circle. 
 
-    #Flip the display
+    #Flip the display - updates the contents of the display to the screen, without this call nothing would appear.
     pygame.display.flip()
 
 # once loop is done - quit
