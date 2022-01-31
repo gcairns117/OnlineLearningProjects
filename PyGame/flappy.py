85a2444b78c3e1a4100677c9b8ac3b1ab4cad22b
gcairns117 <glenncairns96@gmail.com>
2022-01-31 03:39:00 +0000

    added cloud sprites as background detail

--- PyGame/flappy.py
+++ PyGame/flappy.py
@@ -48,8 +48,7 @@ pygame.display.set_icon(icon)
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
-        self.surf.fill((255,255,255))
+        self.surf = pygame.image.load("dove.png").convert_alpha()
         self.rect = self.surf.get_rect(
             center = (SCREEN_WIDTH/2 - 75, SCREEN_HEIGHT/2)
             )
@@ -65,7 +64,7 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_RIGHT] or pressed_keys[pygame.K_d]:
             self.rect.move_ip(5, 0)
     
-    # Keep player on screen
+    # Keep player within screen
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right > SCREEN_WIDTH:
@@ -80,18 +79,18 @@ class Player(pygame.sprite.Sprite):
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self):
         super(Obstacle, self).__init__()
-        self.surf = pygame.Surface((50, 30))
-        self.surf.fill((0,0,0))
+        self.surf = pygame.Surface((50, random.randint(SCREEN_HEIGHT/3, SCREEN_HEIGHT-32)))
+        self.surf.fill((0,128,0))
         # Rect is assigned random location on the screen on the right side, 20 to 100 pixels away
         # It will be somewhere between the bottom and top of screen
         self.rect = self.surf.get_rect(
             center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
-                random.randint(0, SCREEN_HEIGHT)
+                SCREEN_WIDTH + 50,
+                0
+                )
             )
-        )
         # Assign how fast obstacle sprite will move across the screen
-        self.speed = random.randint(1, 5)
+        self.speed = 2
 
 # Move the sprite based on speed 
 # Remove sprite when it passes the left edge of screen
@@ -101,27 +100,69 @@ class Obstacle(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+# Create the bottom obstacle to mirror the top obstacle
+class BottomObstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        super(BottomObstacle, self).__init__()
+        self.surf = pygame.Surface((50, random.randint(SCREEN_HEIGHT/3, SCREEN_HEIGHT-32)))
+        self.surf.fill((255,0,0))
+        self.rect = self.surf.get_rect(
+            center=(
+                SCREEN_WIDTH + 50,
+                SCREEN_HEIGHT
+            )
+        )
+        self.speed = 2
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+# Adding Background Images - using and image for sprite
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load("bigcloud.png").convert_alpha()
+        # Starting position randomly generated
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 40, SCREEN_WIDTH + 45),
+                random.randint(0, SCREEN_HEIGHT)
+            )
+        )
+    # Move the cloud based on constant speed & remove cloud once it has passed left edge of screen
+    def update(self):
+        self.rect.move_ip(-5, 0)
+        if self.rect.right < 0:
+            self.kill()
+
 # Instantiate player object
 player = Player()
 
 # Creating a custom event for creating a new obstacle
 ADDOBSTACLE = pygame.USEREVENT + 1
 # Timer calls ADDOBSTACLE event in specified milliseconds
-pygame.time.set_timer(ADDOBSTACLE, 1000)
+pygame.time.set_timer(ADDOBSTACLE, 2500)
+
+#Creating custom event for clouds
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1500)
 
 # Creating sprite groups to hold all obstacle sprites & another for all sprites
 # Obstacle group to be used for collision dectection & position updates
 # All sprites group to be used for rendering
 obstacles = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
-all_sprites.add(player)
+# New group to hold cloud sprites
+clouds = pygame.sprite.Group()
 
 # Game Loop
 running = True      # Bool control variable to keep game loop running
 while running:      # Starts the event handler
 
     # Fill the background with light blue to replicate the sky
-    screen.fill((173, 216, 230))        # takes tuple rgb colour value
+    screen.fill((135, 206, 250))        # takes tuple rgb colour value
 
     # Loop through every active event
     for event in pygame.event.get():
@@ -135,11 +176,16 @@ while running:      # Starts the event handler
                 running = False
         # Add new obstacle
         elif event.type == ADDOBSTACLE:
-            # Create a new obstacle and add it to sprite groups
-            new_obstacle = Obstacle()
-            obstacles.add(new_obstacle)
-            all_sprites.add(new_obstacle)
-
+            # Create new obstacles and add them to sprite groups
+            t_obstacle = Obstacle()
+            b_obstacle = BottomObstacle()
+            obstacles.add(t_obstacle, b_obstacle)
+            all_sprites.add(t_obstacle, b_obstacle)
+        # Add new cloud
+        elif event.type == ADDCLOUD:
+            newCloud = Cloud()
+            clouds.add(newCloud)
+            all_sprites.add(newCloud)
     
     # Get the set of keys pressed and check for user input
     pressed_keys = pygame.key.get_pressed()
@@ -147,16 +193,23 @@ while running:      # Starts the event handler
     player.update(pressed_keys)
     # Update obstacle location / position
     obstacles.update()
+    # Update clouds
+    clouds.update()
 
     # blit() places top left corner of surface or at location given
-    # Draw all sprites
-    for i in all_sprites:
+    # Draw cloud sprites first before obstacles for the are in background
+    for i in clouds:
         screen.blit(i.surf, i.rect)
+    for i in obstacles:
+        screen.blit(i.surf, i.rect)
+    #blit player after everything else to enure sprite is visible
+    screen.blit(player.surf, player.rect)
+    
 
     #Check for collisions between obstacle and player
     if pygame.sprite.spritecollideany(player, obstacles):
         # If so, stop the game for 1000 milliseconds and end loop
-        pygame.time.wait(5000)
+        pygame.time.wait(1000)
         running = False
 
     #draw the getFPS function to coordinates 0,0 (top left)
